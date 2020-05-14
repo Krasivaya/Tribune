f877eae549eaaf3805664203cf68d6662ee65aa2
Krasivaya <semwagacarine@gmail.com>
2020-05-14 17:55:58 +0200

    Add time stamp

--- news/models.py
+++ news/models.py
@@ -22,6 +22,7 @@ class Article(models.Model):
     post = models.TextField()
     editor = models.ForeignKey(Editor)
     tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add = True)
 
     def __str__(self):
         return self.title
