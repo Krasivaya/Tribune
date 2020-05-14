e1a1dc786ade3b3c166fd49b53da250000383408
Krasivaya <semwagacarine@gmail.com>
2020-05-14 17:52:08 +0200

    Add article model

--- news/models.py
+++ news/models.py
@@ -5,3 +5,12 @@ class Editor(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+
+# Article Model
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor)
