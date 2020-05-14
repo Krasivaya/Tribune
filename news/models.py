be10ac2724719f632b6e8aacc1ab90eb1cccd5c0
Krasivaya <semwagacarine@gmail.com>
2020-05-14 19:48:37 +0200

    Add tests for models

--- news/models.py
+++ news/models.py
@@ -8,13 +8,31 @@ class Editor(models.Model):
 
     def __str__(self):
         return self.first_name
+
+    def save_editor(self):
+        return self.save()
     
+    def update_editor(self):
+        return Editor.objects.filter(id=1).update(first_name='Cary')
+    
+    def delete_editor(self):
+        return Editor.objects.filter(id=1).delete()
+
 #Tag Model
 class tags(models.Model):
     name = models.CharField(max_length=30)
 
     def __str__(self):
         return self.name
+
+    def save_tag(self):
+        return self.save()
+
+    def update_tag(self):
+        return tags.objects.filter(id=1).update(name='Music')
+
+    def delete_tag(self):
+        return tags.objects.filter(id=1).delete()
 
 # Article Model
 class Article(models.Model):
--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,68 @@
 from django.test import TestCase
+from .models import Editor, tags, Article
 
-# Create your tests here.
+# Editor tests
+class EditorTestClass(TestCase):
+    # Setup Method
+    def setUp(self):
+        self.carine = Editor(first_name='Carine', last_name='SEMWAGA', email='semwagacarine@gmail.com')
+
+    # Test instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.carine, Editor))
+
+    # Test save method
+    def test_save_method(self):
+        self.carine.save_editor()
+        editor = Editor.objects.all()
+        self.assertTrue(len(editor) > 0)
+
+    # Test update method
+    def test_update_method(self):
+        self.carine.update_editor()
+
+    # Test delete method
+    def test_delete_method(self):
+        self.carine.delete_editor()
+        editor = Editor.objects.all()
+        self.assertTrue(len(editor) == 0)
+    
+# Tags Tests
+class tagsTestClass(TestCase):
+    # Setup method
+    def setUp(self):
+        self.sports = tags(name='Sports')
+
+    # Test instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.sports, tags))
+
+    # Test save method
+    def test_save_method(self):
+        self.sports.save_tag()
+        tag = tags.objects.all()
+        self.assertTrue(len(tag) > 0)
+
+    # Test update method
+    def test_update_method(self):
+        self.sports.update_tag()
+        tag = tags.objects.all()
+
+    # Test delete method
+    def test_delete_method(self):
+        self.sports.delete_tag()
+        tag = tags.objects.all()
+        self.assertTrue(len(tag)== 0)
+
+# Test Articles
+class ArticleTestClass(TestCase):
+    # Setup method
+    def setUp(self):
+        self.django = Article(
+            title = 'Install Django Framework',
+            post = 'First create a virtualen with python packages'
+        )
+    
+    # Test instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.django, Article))
