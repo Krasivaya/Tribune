7d13d50b8812283b98b33fa07ab5644063d41f42
Krasivaya <semwagacarine@gmail.com>
2020-05-14 17:54:49 +0200

    Add database relationships

--- news/models.py
+++ news/models.py
@@ -8,9 +8,20 @@ class Editor(models.Model):
 
     def __str__(self):
         return self.first_name
+    
+#Tag Model
+class tags(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
 
 # Article Model
 class Article(models.Model):
     title = models.CharField(max_length=100)
     post = models.TextField()
     editor = models.ForeignKey(Editor)
+    tags = models.ManyToManyField(tags)
+
+    def __str__(self):
+        return self.title
