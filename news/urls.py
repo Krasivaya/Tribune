6ef9cd17ab3f7cc7d28bc6bd8a92903dd3210084
Krasivaya <semwagacarine@gmail.com>
2020-05-14 12:09:29 +0200

    Add todays route

--- news/urls.py
+++ news/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^$', views.welcome, name = 'welcome')
+    url('^$', views.welcome, name = 'welcome'),
+    url(r'^today/$', views.news_of_day, name = 'newsToday')
 ]
