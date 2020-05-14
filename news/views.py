60f196aa8116d1e529d53df727bceadc4a39f0d5
Krasivaya <semwagacarine@gmail.com>
2020-05-14 12:08:14 +0200

    Add news of today view function

--- news/views.py
+++ news/views.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse
+import datetime as dt
 
 # Welcome Page
 def welcome(request):
     return HttpResponse(
         'Welcome to the Tribune'
     )
+
+# News of Today
+def news_of_day(request):
+    date = dt.date.today()
+    html = f'''
+                <body>
+                    <h1>
+                        {date.day} - {date.month} - {date.year}
+                    </h1>
+                </body>
+            '''
