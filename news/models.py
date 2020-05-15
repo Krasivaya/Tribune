0f6828ad7c362fd48a37a68669d0fdc58d98f232
Krasivaya <semwagacarine@gmail.com>
2020-05-15 19:01:38 +0200

    Add fullname property

--- news/models.py
+++ news/models.py
@@ -9,7 +9,8 @@ class Editor(models.Model):
     phone_number = models.CharField(max_length=30, blank=True)
 
     def __str__(self):
-        return self.first_name
+        full_name = f'{self.first_name} {self.last_name}'
+        return full_name
 
     def save_editor(self):
         return self.save()
