4ecb573a5966d01517f1b34773d76ba55bbe9270
Krasivaya <semwagacarine@gmail.com>
2020-05-15 16:21:18 +0200

    Add search view fuunction

--- news/views.py
+++ news/views.py
@@ -36,3 +36,24 @@ def past_days_news(request, past_dates):
             "news": news
         }
     )
+
+# Search News 
+def search_results(request):
+    if 'article' in request.GET and request.GET['article']:
+        search_term = request.GET.get('article')
+        searched_articles = Article.search_by_title(search_term)
+        title = f'Searching for {search_term}'
+        message = f"{search_term}"
+
+        return render(
+            request,
+            'all-news/search.html',
+            {
+                "articles": searched_articles,
+                "message": message,
+                "title": title
+            }
+        )
+    else:
+        message = "You haven't searched for any term"
+        return render(request,'all-news/search.html',{"message":message})
