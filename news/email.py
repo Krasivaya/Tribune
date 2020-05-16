14b68bb2b235f056dc7e26e8d5e348d151caf6d5
Krasivaya <semwagacarine@gmail.com>
2020-05-16 20:42:47 +0200

    Add subject and sender variables

--- news/email.py
+++ news/email.py
@@ -3,3 +3,6 @@ from django.template.loader import render_to_string
 
 # Welcome EMail
 def welcome_mail(name, receiver):
+    subject = 'Welcome To The Tribune NewsLetter'
+    sender = 'semwagacarine@gmail.com'
+    
