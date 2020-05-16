287df1d465418c2b480b741cdf76f161353f3bc3
Krasivaya <semwagacarine@gmail.com>
2020-05-16 18:35:16 +0200

    Add email field to form class

--- news/forms.py
+++ news/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 # Create Newsletter Form
-class NewsletterForm(forms.form):
+class NewsletterForm(forms.Form):
     name = forms.CharField(label='Your Name', max_length=30)
-    email = forms.EmailField(labe='Your Email Address')
+    email = forms.EmailField(label='Your Email Address')
