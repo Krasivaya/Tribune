4104cbb3a332250667d497d97d838968833c8e1b
Krasivaya <semwagacarine@gmail.com>
2020-05-16 22:05:51 +0200

    Add registration route

--- tribune/urls.py
+++ tribune/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin, views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('news.urls')),
-    url(r'^accounts/', include(registration.backends.simple.urls)),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^logout/$', views.logout, {"next_page": /})
 ]
