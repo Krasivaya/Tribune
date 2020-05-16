5ac9cf8191fb2ac5e589b1d3e8cb8b63722c4718
Krasivaya <semwagacarine@gmail.com>
2020-05-16 22:05:12 +0200

    Add logout route

--- tribune/urls.py
+++ tribune/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-from django.contrib import admin
+from django.contrib import admin, views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('news.urls')),
-    url(r'^accounts/', include(registration.backends.simple.urls))
+    url(r'^accounts/', include(registration.backends.simple.urls)),
+    url(r'^logout/$', views.logout, {"next_page": /})
 ]
