2bfdce22836883cbce3128ca17230a608ce22d66
Krasivaya <semwagacarine@gmail.com>
2020-05-18 11:32:26 +0200

    Add article post form

--- news/forms.py
+++ news/forms.py
@@ -1,6 +1,16 @@
 from django import forms
+from .models import Article
 
 # Create Newsletter Form
 class NewsletterForm(forms.Form):
     name = forms.CharField(label='Your Name', max_length=30)
     email = forms.EmailField(label='Your Email Address')
+
+# Create Article Post
+class ArticleForm(forms.ModelForm):
+    class meta:
+        model = Article
+        exclude = ['editor', 'pub_date']
+        widgets = {
+            'tags' = checkboxSelectMultiple(),
+        }
